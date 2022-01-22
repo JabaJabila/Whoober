14cb420ca283595cf741e09355935424dc179793
Danilaa56 <danila.aronov@gmail.com>
2022-01-22 16:42:18 +0300

    feat: initial created web presentation project feat: added default testing db context using in memory db

---
+++ WhooberApp/Whoober WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Whoober_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get;
+            set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WhooberApp/WhooberServiceTests/TestingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WhooberInfrastructure.Data;
+
+namespace WhooberServiceTests
+{
+    public class TestingDbContext : WhooberContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("TestingWhooberDbContext");
+        }
+    }
+}
